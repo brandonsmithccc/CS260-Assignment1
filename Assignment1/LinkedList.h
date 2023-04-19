faef21801bb85e0b87f4069b563c8d33449309cf
smithbwlhs <smithb@wlwv.k12.or.us>
2023-04-18 21:36:35 -0700

    done?

--- Assignment1/LinkedList.h
+++ Assignment1/LinkedList.h
@@ -11,29 +11,26 @@
 #include <iostream>
 #include <stdlib.h>
 
+template<typename Type>
 struct Node {
-	int data;
-	Node* next;
+	Type data;
+	Node<Type>* next;
 };
 
+template<class Type>
 class LinkedList {
 public:
-
 	/**
 	 * \brief Constructor for the linked list.
 	 * 
 	 */
-	LinkedList() {
-		head = nullptr;
-	}
+	LinkedList();
 
 	/**
 	 * \brief Destructor for the linked list.
 	 * 
 	 */
-	~LinkedList() {
-		removeAll();
-	}
+	~LinkedList();
 
 	/**
 	 * Inserts the given integer at the beginning of the linked list.
@@ -41,124 +38,35 @@ public:
 	 * \param n integer to insert
 	 * \return true if the item is unique and was inserted, false if not
 	 */
-	bool prepend(const int n) {
-		
-		if (find(n) == 0) { //item not found in list
-			Node* newNode = new Node;
-			newNode->data = n;
-			newNode->next = head;
-			head = newNode;
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-
-		
+	bool prepend(const Type& n);
 
 	/**
-	 * \brief Inserts the given integer at the end of the linked list.
+	 * \brief Inserts the given item at the end of the linked list.
 	 * 
-	 * \param n integer to insert
+	 * \param n item to insert
 	 * \return true if the item is unique and was inserted, false otherwise
 	 */
-	bool append(const int n) {
-		if (find(n) == 0) {
-			Node* current = head;
-			Node* newNode = new Node;
-
-			newNode->data = n;
-			newNode->next = nullptr;
-
-			if (head == nullptr) {
-				prepend(n);
-			}
-
-			else {
-				while (current->next != nullptr) { //have to stop one before the final node
-					current = current->next;
-				}
-				current->next = newNode;
-				return true;
-			}
-		}
-		
-		return false;
-	}
+	bool append(const Type& n);
 
 	/**
-	 * \brief Removes the given integer from the list.
+	 * \brief Removes the given item from the list.
 	 * 
-	 * \param n integer to remove
-	 * \return true if integer was found and removed, false otherwise
+	 * \param n datatype item to remove
+	 * \return true if item was found and removed, false otherwise
 	 */
-	bool remove(const int n) {
-		Node* current = head;
-		Node* previous = nullptr;
-
-		if (head == nullptr) { //if the list is empty
-			return false;
-		}
-
-		if (find(n) == 1) { //if item to remove is first
-			head = head->next;
-			delete current;
-			return true;
-		}
-
-		while (current != nullptr) { //could I use a for loop here since I have the find function?
-			if (current->data == n) {
-				previous->next = current->next;
-				delete current;
-				return true;
-			}
-			previous = current;
-			current = current->next;
-		}
-		return false;
-	}
+	bool remove(const Type& n);
 
 	/**
 	 * Prints the number of items in the list followed by the items in the list.
 	 * 
 	 */
-	void print() {
-		int length = count();
-		if (head == nullptr) {
-			std::cout << "The list contains 0 elements." << std::endl;
-			exit(0);
-		}
-		else if (count() == 1) {
-			std::cout << "The list contains 1 element." << std::endl;
-		}
-		else {
-			std::cout << "The list contains " << length << " elements." << std::endl;
-
-		}
-
-		Node* current = head;
-		Node* nextNode = current->next;
-		for (int i = 0; i < length - 1; i++) {
-			std::cout << current->data << ", ";
-			current = current->next;
-		}
-		std::cout << current->data << std::endl;
-	}
+	void print();
 
 	/**
 	 * 
 	 * \return the number of integers in the list
 	 */
-	int count() {
-		Node* current = head;
-		int count = 0;
-		while (current != nullptr) {
-			count++;
-			current = current->next;
-		}
-		return count;
-	}
+	int count();
 
 	/**
 	 * Find the given integer in the linked list.
@@ -166,35 +74,153 @@ public:
 	 * \param n the integer to find
 	 * \return the position of the integer in the list (1 indexed), -1 if not found
 	 */
-	int find(const int n) {
-		Node* current = head;
-
-		int position = 1;
-		while (current != nullptr) {
-			if (current->data == n) {
-				return position;
-			}
-			position++;
-			current = current->next;
-		}
-		return 0;
-	}
+	int find(const Type& n);
 
 	/**
 	 * Removes all items in the linked list.
 	 * 
 	 */
-	void removeAll() {
-		Node* current = head;
-		while (current != nullptr) {
-			Node* nodeToDelete = current;
-			current = current->next;
-			delete nodeToDelete;
+	void removeAll();
+
+private:
+	Node<Type>* head;
+};
+
+template<class Type>
+LinkedList<Type>::LinkedList() {
+	head = nullptr;
+}
+
+template<class Type>
+LinkedList<Type>::~LinkedList() {
+	removeAll();
+}
+
+template<class Type>
+bool LinkedList<Type>::prepend(const Type& n) {
+	if (find(n) == 0) { //item not found in list
+		Node<Type>* newNode = new Node<Type>;
+		newNode->data = n;
+		newNode->next = head;
+		head = newNode;
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+template<class Type>
+bool LinkedList<Type>::append(const Type& n) {
+	if (find(n) == 0) {
+		Node<Type>* current = head;
+		Node<Type>* newNode = new Node<Type>;
+
+		newNode->data = n;
+		newNode->next = nullptr;
+
+		if (head == nullptr) {
+			prepend(n);
 		}
-		head = nullptr;
+
+		else {
+			while (current->next != nullptr) { //have to stop one before the final node
+				current = current->next;
+			}
+			current->next = newNode;
+			return true;
+		}
 	}
 
-	
-private:
-	Node* head;
-};
+	return false;
+}
+
+template<class Type>
+bool LinkedList<Type>::remove(const Type& n) {
+	Node<Type>* current = head;
+	Node<Type>* previous = head;
+
+	if (head == nullptr) { //if the list is empty
+		return false;
+	}
+
+	if (find(n) == 1) { //if item to remove is first
+		head = head->next;
+		delete current;
+		return true;
+	}
+
+	while (current != nullptr) { //could I use a for loop here since I have the find function?
+		if (current->data == n) {
+			previous->next = current->next;
+			delete current;
+			return true;
+		}
+		previous = current;
+		current = current->next;
+	}
+	return false;
+}
+
+template<class Type>
+void LinkedList<Type>::print() {
+	int length = this->count();
+
+	if (length == 0) {
+		std::cout << "The list contains 0 elements." << std::endl;
+	}
+	else if (length == 1) {
+		std::cout << "The list contains 1 element." << std::endl;
+	}
+	else {
+		std::cout << "The list contains " << length << " elements." << std::endl;
+
+	}
+
+	Node<Type>* current = this->head;
+	while(current != nullptr) {
+		std::cout << current->data;
+		if (current->next != nullptr) {
+			std::cout << ", ";
+		}
+		current = current->next;
+	}
+	std::cout << std::endl;
+}
+
+template<class Type>
+int LinkedList<Type>::count() {
+	Node<Type>* current = head;
+	int count = 0;
+	while (current != nullptr) {
+		count++;
+		current = current->next;
+	}
+	return count;
+}
+
+template<class Type>
+int LinkedList<Type>::find(const Type& n) {
+	Node<Type>* current = head;
+
+	int position = 1;
+	while (current != nullptr) {
+		if (current->data == n) {
+			return position;
+		}
+		position++;
+		current = current->next;
+	}
+	return 0;
+}
+
+template<class Type>
+void LinkedList<Type>::removeAll() {
+	Node<Type>* current = head;
+	while (current != nullptr) {
+		Node<Type>* nodeToDelete = current;
+		current = current->next;
+		delete nodeToDelete;
+	}
+	head = nullptr;
+}
--- Assignment1/LinkedListRunner.cpp
+++ Assignment1/LinkedListRunner.cpp
@@ -9,21 +9,57 @@
  *********************************************************************/
 
 #include "LinkedList.h"
+#include "Student.h"
 #include <iostream>
+#include<string>
+
+const std::string YOUR_NAME = "Brandon Smith";
 
 
 
 int main()
 {
-    LinkedList intList;
+ //   LinkedList<int> intList;
+	//intList.prepend(10);
+	//intList.prepend(20);
+	//intList.prepend(20);
+	//intList.print();
+	//intList.prepend(5);
+	//intList.append(15);
+	//intList.append(15);
+	//intList.print();
+	//intList.append(25);
+	//intList.print();
+
+	//std::cout << "5 is in position " << intList.find(5) << std::endl;
+	//std::cout << "15 is in position " << intList.find(15) << std::endl;
+	//std::cout << "20 is in position " << intList.find(20) << std::endl;
+	//std::cout << "8 is in position " << intList.find(8) << std::endl;
+
+	//intList.remove(15);
+	//intList.print();
+	//intList.remove(5);
+	//intList.print();
+	//intList.remove(25);
+	//intList.print();
+
+	//intList.removeAll();
+	////intList.print();
+	//std::cout << "NEW LIST..." << std::endl << std::endl;
+
+	//LinkedList<char> charList;
+	//charList.prepend('a');
+	//charList.prepend('b');
+	//charList.print();
+
+	std::cout << YOUR_NAME << std::endl;
+
+	// test template with ints --------------------------------
+	LinkedList<int> intList;
 	intList.prepend(10);
 	intList.prepend(20);
-	intList.prepend(20);
-	intList.print();
 	intList.prepend(5);
 	intList.append(15);
-	intList.append(15);
-	intList.print();
 	intList.append(25);
 	intList.print();
 
@@ -33,15 +69,60 @@ int main()
 	std::cout << "8 is in position " << intList.find(8) << std::endl;
 
 	intList.remove(15);
-	intList.print();
 	intList.remove(5);
-	intList.print();
 	intList.remove(25);
 	intList.print();
 
 	intList.removeAll();
+	intList.append(2);
 	intList.print();
-	std::cout << std::endl << std::endl;
+	std::cout << std::endl << "Char List" << std::endl;
+
+	// test on list of chars --------------------------------
+	LinkedList<char>* charList = new LinkedList<char>;
+	charList->append('k');
+	charList->append('s');
+	charList->prepend('r');
+	charList->prepend('o');
+	charList->prepend('w');
+	charList->print();
+
+	charList->remove('r');
+	charList->remove('w');
+	charList->remove('s');
+	charList->print();
+
+	charList->removeAll();
+	charList->print();
+	delete charList;
+
+	std::cout << std::endl << "Student List" << std::endl;
+	LinkedList<Student>* studentList = new LinkedList<Student>;
+	Student Brandon("Brandon", 'A');
+	studentList->append(Brandon);
+	std::cout << Brandon << std::endl;
+	std::cout << "Print list" << std::endl;
+	studentList->print();
+	Student John("John", 'B');
+	studentList->prepend(John);
+	std::cout << "Print list" << std::endl;
+	studentList->print();
+	studentList->remove(Brandon);
+	std::cout << "Print list" << std::endl;
+	studentList->print();
+	std::cout << "Position of John is " << studentList->find(John) << std::endl;
+	std::cout << "Position of Brandon is " << studentList->find(Brandon) << std::endl;
+	Student JohnB("John", 'B');
+	studentList->append(JohnB);
+	Student AliceA("Alice", 'A');
+	studentList->append(AliceA);
+	std::cout << "Print list" << std::endl;
+	studentList->print();
+	std::cout << "John and JohnB are the same: " << std::boolalpha << (John == JohnB) << std::endl;
+	std::cout << "John and AliceA are the same: " << std::boolalpha << (JohnB == AliceA) << std::endl;
+
+
+
 
 
 
